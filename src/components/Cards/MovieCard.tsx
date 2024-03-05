a16d9353a89419acb90a8429d76eab02c88008f9
Mitka Panarin <mitka_j@yahoo.com>
2024-03-06 04:55:11 +0600

    get 1 movie details added

--- src/components/Cards/MovieCard.tsx
+++ src/components/Cards/MovieCard.tsx
@@ -22,7 +22,7 @@ import { genreOptions } from "@/types/options";
 
 const MovieCard: FC<
   IMoviesData & {
-    deleteFn: (id: string) => void;
+    deleteFn?: (id: string) => void;
     updateFn: (data: IMoviesData) => void;
     reviewFn: (
       id: string,
@@ -65,90 +65,95 @@ const MovieCard: FC<
         <CardTitle className="flex justify-between items-center">
           <h1 className="capitalize">{title}</h1>
           <div className="flex gap-2">
-            <EventModal
-              dialogueTitle="Edit Movie"
-              confirmButtonText="Update Movie"
-              dialogueDescription="Once you're done, click on the button below to update the movie details."
-              onConfirm={() =>
-                updateFn({
-                  ...data,
-                  _id,
-                }) as any
-              }
-              button={
-                <Button size="sm" variant="secondary" className="text-sm">
-                  <MdModeEdit className="w-4 h-4" />
-                </Button>
-              }
-            >
-              <div className="flex flex-col gap-2 my-4">
-                <InputField
-                  label="Movie Title"
-                  name="title"
-                  placeholder="Enter Movie Title"
-                  type="text"
-                  description="Enter the title of the movie you want to add."
-                  required
-                  onChange={(e) =>
-                    setData({
-                      ...data,
-                      title: e.target.value,
-                    })
-                  }
-                  value={data.title}
-                />
-                <InputField
-                  label="Publish Year"
-                  name="year"
-                  placeholder="Enter Publish Year"
-                  type="number"
-                  description="When was the movie published? Enter the year here."
-                  required
-                  onChange={(e) =>
-                    setData({
-                      ...data,
-                      year: Number(e.target.value),
-                    })
-                  }
-                  value={data.year}
-                />
-                <SelectField
-                  label="Genre"
-                  onValueChange={(e: GenreEnum) =>
-                    setData({
-                      ...data,
-                      genre: e,
-                    })
-                  }
-                  options={genreOptions}
-                  value={data.genre}
-                  required
-                />
-                <InputField
-                  label="Description"
-                  name="description"
-                  placeholder="Enter Description"
-                  type="text"
-                  description="Write a short description of the movie you want to add."
-                  required
-                  onChange={(e) =>
-                    setData({
-                      ...data,
-                      description: e.target.value,
-                    })
-                  }
-                  value={data.description}
-                />
-              </div>
-            </EventModal>
-            <Button
-              onClick={() => deleteFn(_id)}
-              size="sm"
-              variant="destructive"
-              className="text-xs"
-            >
-              <FaRegTrashAlt className="w-4 h-4" />
-            </Button>
+            {updateFn && (
+              <EventModal
+                dialogueTitle="Edit Movie"
+                confirmButtonText="Update Movie"
+                dialogueDescription="Once you're done, click on the button below to update the movie details."
+                onConfirm={() =>
+                  updateFn({
+                    ...data,
+                    _id,
+                  }) as any
+                }
+                button={
+                  <Button size="sm" variant="secondary" className="text-sm">
+                    <MdModeEdit className="w-4 h-4" />
+                  </Button>
+                }
+              >
+                <div className="flex flex-col gap-2 my-4">
+                  <InputField
+                    label="Movie Title"
+                    name="title"
+                    placeholder="Enter Movie Title"
+                    type="text"
+                    description="Enter the title of the movie you want to add."
+                    required
+                    onChange={(e) =>
+                      setData({
+                        ...data,
+                        title: e.target.value,
+                      })
+                    }
+                    value={data.title}
+                  />
+                  <InputField
+                    label="Publish Year"
+                    name="year"
+                    placeholder="Enter Publish Year"
+                    type="number"
+                    description="When was the movie published? Enter the year here."
+                    required
+                    onChange={(e) =>
+                      setData({
+                        ...data,
+                        year: Number(e.target.value),
+                      })
+                    }
+                    value={data.year}
+                  />
+                  <SelectField
+                    label="Genre"
+                    onValueChange={(e: GenreEnum) =>
+                      setData({
+                        ...data,
+                        genre: e,
+                      })
+                    }
+                    options={genreOptions}
+                    value={data.genre}
+                    required
+                  />
+                  <InputField
+                    label="Description"
+                    name="description"
+                    placeholder="Enter Description"
+                    type="text"
+                    description="Write a short description of the movie you want to add."
+                    required
+                    onChange={(e) =>
+                      setData({
+                        ...data,
+                        description: e.target.value,
+                      })
+                    }
+                    value={data.description}
+                  />
+                </div>
+              </EventModal>
+            )}
+
+            {deleteFn && (
+              <Button
+                onClick={() => deleteFn(_id)}
+                size="sm"
+                variant="destructive"
+                className="text-xs"
+              >
+                <FaRegTrashAlt className="w-4 h-4" />
+              </Button>
+            )}
           </div>
         </CardTitle>
         <CardDescription className="">{description}</CardDescription>
@@ -156,7 +161,7 @@ const MovieCard: FC<
       <CardContent className="p-4 py-0 flex gap-2 flex-wrap">
         <Button variant="outline" size="sm" className="flex gap-1">
           <CiStar className="w-6 h-6" />
-          <p>{rating.toFixed(1)}</p>
+          <p>{+rating ? +rating?.toFixed(1) : 0}</p>
         </Button>
         <Button variant="outline" size="sm" className="flex gap-1">
           <CiCalendar className="w-6 h-6" />
--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,5 +1,71 @@
+import { useParams, Link } from "react-router-dom";
+import {
+  useGetOneMovieByIdQuery,
+  useSubmitReviewMutation,
+} from "@/store/store";
+import MovieCard from "@/components/Cards/MovieCard";
+import LoadingUI from "@/components/Skeleton/LoadingUI";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { IMoviesData } from "@/types/interface";
+
 const MovieDetails = () => {
-  return <div>MovieDetails</div>;
+  const { id } = useParams<{ id: string }>();
+  const [submitReview] = useSubmitReviewMutation();
+  const { data, isError, isLoading, isFetching } = useGetOneMovieByIdQuery({
+    _id: id as string,
+  });
+
+  const submitReviewFn = async (
+    _id: string,
+    reviewState: {
+      review: string;
+      rating: number;
+    },
+  ) =>
+    toast.promise(submitReview({ _id, ...reviewState }).unwrap(), {
+      loading: "Submitting Review...",
+      success: "Review Submitted Successfully",
+      error: "Error Occured, please try again later",
+    });
+  console.log(data);
+  if (isLoading || isFetching) {
+    return <LoadingUI />;
+  }
+  if (isError) {
+    return <div>Error occured, please try again</div>;
+  }
+  return (
+    <div className="container max-w-8xl">
+      <Link to="/">
+        <Button>Go Back</Button>
+      </Link>
+      <div className="my-4 grid grid-cols-1 md:grid-cols-2 gap-10 justify-center">
+        {/* @ts-ignore */}
+        <MovieCard {...(data as IMoviesData)} reviewFn={submitReviewFn} />
+        <div className="">
+          <h1 className="text-2xl font-semibold mt-4 mb-8">
+            Reviews From People
+          </h1>
+          <ul className="list-disc">
+            {data?.stats?.review?.length == 0 ? (
+              <div className="">
+                <p>No Reviews Found, Be the first one to review this !</p>
+              </div>
+            ) : (
+              data?.stats?.review?.map((item, index) => {
+                return (
+                  <li key={index} className="list-item">
+                    {item}
+                  </li>
+                );
+              })
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetails;
