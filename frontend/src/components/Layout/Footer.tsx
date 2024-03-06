81d6876b559c65eeca1bab0bcf733bf75c9951b8
Mitka Panarin <114948515+mitkapanarin@users.noreply.github.com>
2024-03-06 15:23:45 +0100

    .

--- frontend/src/components/Layout/Footer.tsx
+++ frontend/src/components/Layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="container max-w-8xl text-center">
-      <h1 className="text-2xl my-10">Made by Mitka Maneva</h1>
+      <h1 className="text-2xl my-10">Made by Mitka Panarin</h1>
     </div>
   );
 };
